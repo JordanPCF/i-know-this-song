5fa3d8399d4dda64bd3cbfc7d9b83a70019468eb
jp <jordanpflem@gmail.com>
2021-04-13 16:55:45 -0700

    testing 'genius' search

--- lambda-scripts/lambda_function.py
+++ lambda-scripts/lambda_function.py
@@ -1,6 +1,9 @@
-from spotify_playlists import get_user_playlists
-from genius import get_song_from_lyrics
+from spotify import get_user_playlists
+from genius import get_songs_from_lyrics
+from data_collector import get_song_set
+from playlist import Playlist
 import json
+import re
 import boto3
 from botocore.exceptions import ClientError
 import logging
@@ -37,6 +40,31 @@ def lambda_handler(event, context):
             status_code = 460
             
     elif body['requestType'] == 'lyrics':
+        # TODO: abstract this code out elsewhere, clean it up
+        # currently solving for ONE playlist given, as a string (PLAYLIST_NAME: ID)
+        playlists_ = re.findall(r'(.+):\ (\w+)', body['playlists'])
+        logger.info(f"playlists_ = {playlists_}")
+        playlist = Playlist(playlists_[0][0], playlists_[0][1])
+        set_of_songs = get_song_set([playlist])
         
+        message = ""
+        genius_songs = get_songs_from_lyrics(body['lyrics'])
+        for song in genius_songs:
+            if song in set_of_songs:
+                message = f"Found match! {song}".encode('utf-8')
+                
+        if message == "":
+            message = "No matches found".encode('utf-8')
+        logger.info(f"lyric match message: {message}")
+        
+        try: 
+            send_response = apig_management_client.post_to_connection(Data=message, ConnectionId=connection_id)
+            logger.info(f"Posted message to connection {connection_id}, got response {send_response}")
+        except ClientError:
+            logger.exception(f"Couldn't post to connection {connection_id}")
+            status_code = 450
+        except apig_management_client.exceptions.GoneException:
+            logger.info(f"Connection {connection_id} is gone, removing.")
+            status_code = 460
     
     return status_code
